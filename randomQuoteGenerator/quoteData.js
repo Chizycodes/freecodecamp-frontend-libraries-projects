1cf13f03161ca9eb9363dda590c8cdfb792d7778
Joy Okwudire <okwudirejoy@gmail.com>
2021-09-27 04:15:00 +0100

    Added tweet function

---
+++ randomQuoteGenerator/quoteData.js
@@ -0,0 +1,80 @@
+// Array of quote texts and authors
+export let quoteData = [
+  {
+    text: "We write to taste life twice, in the moment and in retrospect.",
+    author: "- Anaïs Nin",
+  },
+  {
+    text:
+      "Even if we don't have the power to choose where we come from, we can still choose where we go from there.",
+    author: "- Stephen Chbosky",
+  },
+  {
+    text: "And now that you don't have to be perfect, you can be good.",
+    author: "- John Steinbeck",
+  },
+  {
+    text:
+      "We are cups, constantly and quietly being filled. The trick is knowing how to tip ourselves over and let the beautiful stuff out.",
+    author: "- Ray Bradbury",
+  },
+  {
+    text:
+      "Life is never fair, and perhaps it is a good thing for most of us that it is not.",
+    author: "- Oscar Wilde",
+  },
+  {
+    text:
+      "Monsters are real, ghosts are real, too. They live inside us, and sometimes they win.",
+    author: "- Stephen King",
+  },
+  {
+    text:
+      "We are all ordinary. We are all boring. We are all spectacular. We are all shy. We are all bold. We are all heroes. We are all helpless. It just depends on the day.",
+    author: "- Brad Meltzer",
+  },
+  {
+    text:
+      "Some of us think holding on makes us strong; but sometimes it is letting go.",
+    author: "- Herman Hesse",
+  },
+];
+
+//Array of colors
+export let colors = [
+  "#FF6633",
+  "#FFB399",
+  "#FF33FF",
+  "#FFFF99",
+  "#00B3E6",
+  "#E6B333",
+  "#3366E6",
+  "#999966",
+  "#99FF99",
+  "#B34D4D",
+  "#80B300",
+  "#809900",
+  "#E6B3B3",
+  "#6680B3",
+  "#66991A",
+  "#FF99E6",
+  "#CCFF1A",
+  "#FF1A66",
+  "#E6331A",
+  "#33FFCC",
+  "#66994D",
+  "#B366CC",
+  "#4D8000",
+  "#B33300",
+  "#CC80CC",
+  "#66664D",
+  "#991AFF",
+  "#E666FF",
+  "#4DB3FF",
+  "#1AB399",
+  "#E666B3",
+  "#33991A",
+  "#CC9999",
+  "#B3B31A",
+  "#00E680",
+];
